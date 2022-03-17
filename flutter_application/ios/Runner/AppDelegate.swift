25a52bb977fa96afde71a2d5f3c22a23ed9efb2b
AungMyoKyaw4198 <aungmyokyaw4198@gmail.com>
2022-03-17 10:06:46 +0630

    Commiting Both apps

---
+++ flutter_application/ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Flutter
+import GoogleMaps
+import Firebase
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCvg_PP20G1i9sMUtSqbrE4NwkkBBbwIcI")
+    FirebaseApp.configure() //add this before the code below
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+}
